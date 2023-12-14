9ba7c0b5ce582770e09f6974d03d1fdc7af13095
Reiner Bielert <bielertwreiner@gmail.com>
2023-12-14 16:49:29 +0000

    updated admin in apps

--- apps/donor_management/admin.py
+++ apps/donor_management/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from .models import Donor, LeadType, LeadAcquisition
 
 # Register your models here.
-admin.register(Donor, LeadType, LeadAcquisition)
+admin.site.register(Donor)
+admin.site.register(LeadType)
+admin.site.register(LeadAcquisition)
